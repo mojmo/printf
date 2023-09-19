3b1ac536be21b533eb8d8bc21ac88b466c143dc8
mojtaba <mojtaba.mohamed187@gmail.com>
2023-09-19 10:46:35 -0700

    fix a condition

--- _printf.c
+++ _printf.c
@@ -18,8 +18,7 @@ int _printf(const char *format, ...)
 
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-
-	if (format[0] == '%' && format[1] == '!')
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
 	if (format[0] == '%' && !spec_check(format[1]))
 		return (-1);

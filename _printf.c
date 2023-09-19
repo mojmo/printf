224136e588b0eb37c955a8cacbe6dbffce21dbb8
Eilla-7 <eilafadel2238@gmail.com>
2023-09-19 10:55:09 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -20,8 +20,6 @@ int _printf(const char *format, ...)
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-	if (format[0] == '%' && !spec_check(format[1]))
-		return (-1);
 
 	while (form && *form)
 	{
@@ -39,23 +37,3 @@ int _printf(const char *format, ...)
 	va_end(args);
 	return (printed_chars);
 }
-/**
- * spec_check - check the specifier
- * @ch: the specifier
- * Return: 1 if ch is a specifier 0 if not
- */
-int spec_check(char ch)
-{
-	char spec[] = "cdibuoxXSrR%";
-	int i, res;
-
-	for (i = 0; i < 13; i++)
-	{
-		if (spec[i] == ch)
-			res = 1;
-		else
-			res = 0;
-	}
-	return (res);
-}
-

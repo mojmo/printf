b1c3630244e514b266b8988347192e34922bcec3
mojtaba <mojtaba.mohamed187@gmail.com>
2023-09-19 19:53:30 -0700

    updated

--- _printf.c
+++ _printf.c
@@ -26,10 +26,7 @@ int _printf(const char *format, ...)
 		if (*form == '%')
 		{
 			form++;
-			if (specifier(form) || specifier(++form))
 			printed_chars += get_func(form, args);
-			if (!specifier(form))
-				return (-1);
 		}
 		else
 			printed_chars += fill_buffer(*form);
